c68f74812e40a68e624d7496945b6e1e20eab0e8
dorberu <dorberu@gmail.com>
2016-09-11 04:30:34 +0900

    PhotonSampleを仮作成

---
+++ PhotonSample/Assets/Resources/manage.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class manage : MonoBehaviour {
+
+	private bool keyLock;
+
+	// Use this for initialization
+	void Start () {
+		keyLock = false;
+		PhotonNetwork.ConnectUsingSettings (null);
+	}
+
+	void OnJoinedLobby() {
+		PhotonNetwork.JoinRandomRoom ();
+	}
+
+	void OnJoinedRoom() {
+		keyLock = true;
+	}
+
+	void OnPhotonRandomJoinFailed() {
+		PhotonNetwork.CreateRoom (null);
+	}
+
+	void FixedUpdate () {
+		if (Input.GetMouseButtonDown (0) && keyLock) {
+			GameObject mySyncObj = PhotonNetwork.Instantiate (
+				"Cube",
+				new Vector3(9.0f, 0.0f, 0.0f),
+				Quaternion.identity,
+				0
+			);
+			Rigidbody mySyncObjRB = mySyncObj.GetComponent<Rigidbody> ();
+			mySyncObjRB.isKinematic = false;
+			float rndPow = Random.Range (1.0f, 5.0f);
+			mySyncObjRB.AddForce (Vector3.left * rndPow, ForceMode.Impulse);
+		}
+	}
+}
